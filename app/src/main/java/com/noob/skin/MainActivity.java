c5ddb5d60f25d5f052cb7296e7f20e37d10441d2
xiaoqi <812817883@qq.com>
2018-09-04 16:19:24 +0800

    upload images

--- app/src/main/java/com/noob/skin/MainActivity.java
+++ app/src/main/java/com/noob/skin/MainActivity.java
@@ -1,34 +1,29 @@
 package com.noob.skin;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.content.res.AssetManager;
-import android.content.res.Resources;
-import android.os.Environment;
-import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
-
-import java.io.File;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-import dalvik.system.DexClassLoader;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    int themeState = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        themeState = getIntent().getIntExtra("themeState", 0);
+        if(themeState == 0){
+            setTheme(R.style.LightTheme);
+        }else {
+            setTheme(R.style.BlackTheme);
+        }
         LayoutInflater.from(this).setFactory(new LayoutInflater.Factory() {
             @Override
             public View onCreateView(String name, Context context, AttributeSet attrs) {
@@ -42,26 +37,31 @@ public class MainActivity extends Activity {
         });
 
         setContentView(R.layout.activity_main);
-    }
 
-//    @Override
-//    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
-//        Log.e("MainActivity2", name);
-//        int count = attrs.getAttributeCount();
-//        for (int i = 0; i < count; i++) {
-//            Log.e("MainActivity2", "AttributeName :" + attrs.getAttributeName(i) + "AttributeValue :"+ attrs.getAttributeValue(i));
-//        }
-//        return super.onCreateView(parent, name, context, attrs);
+        //listView中使用
+        LayoutInflater.from(this).inflate(R.layout.item, null);
+
+
+
+
+//        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//                Intent intent = getIntent();
+//                if(themeState == 0){
+//                    intent.putExtra("themeState", 1);
+//                }else {
+//                    intent.putExtra("themeState", 0);
+//                }
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                startActivity(intent);
+//                overridePendingTransition(0, 0);
+//            }
+//        });
 //
-//    }
 //
-//    @Override
-//    public View onCreateView(String name, Context context, AttributeSet attrs) {
-//        Log.e("MainActivity", name);
-//        int count = attrs.getAttributeCount();
-//        for (int i = 0; i < count; i++) {
-//            Log.e("MainActivity", "AttributeName :" + attrs.getAttributeName(i) + "AttributeValue :"+ attrs.getAttributeValue(i));
-//        }
-//        return super.onCreateView(name, context, attrs);
-//    }
+//        ResourceLoad.loadUnInstallApkRes(this, (ImageView) findViewById(R.id.image));
+
+    }
 }
